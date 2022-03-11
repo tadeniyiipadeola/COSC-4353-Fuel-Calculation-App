3cc8e85db3dc41d8aece5a9185f3d3f55cdf999e
Temi <stadeniy@cougarnet.uh.edu>
2022-03-11 10:16:32 -0600

    Models are built

--- REACT-DJANGO/Fuel_Calc_WebApp/api/models.py
+++ REACT-DJANGO/Fuel_Calc_WebApp/api/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class User(models.Model):
+    id = models.IntegerField(primary_key=True)
+    first_name = models.CharField(max_length=20)
+    last_name = models.CharField(max_length=20)
+    Username = models.CharField(max_length=15, unique=True)
+    Password = models.CharField(unique=True, max_length=20)
+    created_at = models.DateTimeField(auto_now_add=True)
--- REACT-DJANGO/Fuel_Calc_WebApp/api/urls.py
+++ REACT-DJANGO/Fuel_Calc_WebApp/api/urls.py
@@ -1,7 +1,7 @@
 
 from django.urls import path
-from .views import main
+from .views import UserView
 urlpatterns = [
 
-    path('', main)
+    path('home', UserView.as_view())
 ]
